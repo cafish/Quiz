2e92dd27c2a8acdaa7d4ad4b180b3962f5ffbcad
cafish <jiajialuo@DN0a228143.SUNet>
2015-05-20 14:50:56 -0700

    Update XIB file with autolayout

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let questions: [String] = ["1+1?",
+        "What is 2+2?",
+        "3+3?",
+        "What is Pi"]
+    
+    let answers: [String] = ["2",
+            "4",
+            "6",
+            "3.1415926"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    
+    @IBAction func showQuestion(sender: AnyObject) {
+            ++currentQuestionIndex
+            if currentQuestionIndex == questions.count {
+        currentQuestionIndex = 0
+            }
+            let question: String = questions[currentQuestionIndex]
+            questionLabel.text = question
+            answerLabel.text = "???"
+    }
+    
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
